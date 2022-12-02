846e5407a6ca407bc40e67fd380f961ce4ae2b64
Felicity Graham <Felicity.rose.graham@outlook.com>
2022-12-02 11:41:12 -0800

    button operations

--- script.js
+++ script.js
@@ -6,15 +6,26 @@ class Calculator {
 
 
 clear() {
-
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined
 }
 
 delete() {
 
 }
 
-appendNumber() {
+appendNumber(number) {
+ this.currentOperand = number
+}
+chooseOperation(operation) {
 
+}
+compute() {
+
+}
+updateDisplay() {
+    this.currentOperandandTextElement.innerText = this.currentOperand
 }
 }
 const numberButtons = document.querySelectorAllz('[data-number]')
@@ -24,3 +35,14 @@ const deleteButton = document.querySelector('[data-equals]')
 const allClearButton = document.querySelector('[data-equals]')
 const previousOperandandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandandTextElement = document.querySelector('[data-current-operand]')
+
+
+const Calculator = new Calculator (previousOperandandTextElement, currentOperandandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener ('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+    })
+
+})
